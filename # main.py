11bb3a8f0b9c42813e88c295de187cdb0ac5e7c7
Danielsanchez03 <dsanche05703@universidadean.edu.co>
2025-09-02 02:40:08 +0000

    inicio del juego, evaluar saldo y apuesta, repartir cartas y evaluar mano del jugador

--- # main.py
+++ # main.py
@@ -17,16 +17,34 @@ suma_fecha = dia + mes + año
 #hacemos un multiplicador random de 10, 20 o 30
 multiplicador = random.choice([10 , 20 , 30])
 #calculamos el saldo final operando la suma por el multiplicador random
-saldo_final = suma_fecha * multiplicador
+saldo = suma_fecha * multiplicador
 
 #mostramos el saldo despues de la operación y el multiplicador obtenido
 print("felicidades obtubiste tu multiplicador es de:  " , multiplicador)
-print("Tu saldo es de:   " , saldo_final)
+print("Tu saldo es de:   " , saldo)
 
 #definimos la función para pedir una carta
 def pedir():
     return random.choice(Baraja_blackjack)
 
-#función para calcular el puntaje de la mano
+#definimos la función para calcular el puntaje de la mano. suma el valor numérico de cada carta (posición 0 de la lista [valor,palo])
 def calcular_puntaje(mano):
     return sum([carta[0]for carta in mano])
+
+#-----------Inicia el juego--------------
+#Hacemos el ciclo principal del juego y se repite mientras el saldo sea positivo
+while saldo > 0:
+    print("Tu saldo actual es de:  " , saldo)
+    apuesta = int(input("¿Uanto quieres apostar?  "))
+    if apuesta > saldo:
+        print("No puedes apostar mas de tu saldo.")
+        continue
+
+#repartimos cartas iniciales (dos para el jugador y dos para el dealer)
+mano_jugador = [pedir(), pedir()]
+mano_dealer = [pedir(), pedir()]
+
+#mostramos las cartas del jugador y la carta visible del dealer
+print("tus cartas son:  " , mano_jugador)
+print("tu puntaje actual es:  " , calcular_puntaje(mano_jugador))
+print("la carta visible del dealer es: " , mano_dealer)
